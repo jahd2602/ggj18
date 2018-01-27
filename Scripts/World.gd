179eecf1be9d40fe52ddb61d1ee06a641ce66ec3
guilhermevieira <xgvieira@gmail.com>
2018-01-27 12:49:02 -0200

    Add signal quality label and pause button

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -4,12 +4,15 @@ var signal_strength = 0
 const BLOCKING_GROUP = "blocking"
 
 func _ready():
+	randomize()
 	pass
 
 func _process(delta):
 	pass
 
-
 func _on_TransmissionLine_strength_changed(new_strength):
-	signal_strength = new_strength
-	print(signal_strength)
+	if signal_strength != new_strength:
+		signal_strength = new_strength
+		get_node("HUD/Label").set_text("SIGNAL QUALITY " + str(signal_strength) + "%")
+		
+	
