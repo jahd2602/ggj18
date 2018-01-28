efe89a20ff6c1fa79c9cac1f258d0429dc9b1ec8
Jairo Honorio <registrofx@gmail.com>
2018-01-28 13:18:17 -0200

    Menu buttons actions completed

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -4,6 +4,9 @@ extends Control
 # var a = 2
 # var b = "textvar"
 
+export(NodePath) var credits_containers
+export(NodePath) var credits_animation_player
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -14,7 +17,18 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
-
-func _on_Button_pressed():
+func _on_ButtonPlay_pressed():
 	get_tree().change_scene("res://Scenes/World.tscn")
-	pass # replace with function body
+
+
+func _on_ButtonCredits_pressed():
+	get_node(credits_containers).show()
+	get_node(credits_animation_player).play("Credits Go Up")
+
+
+func _on_ButtonExit_pressed():
+	get_tree().quit()
+
+
+func _on_AnimationPlayer_animation_finished( anim_name ):
+	get_node(credits_containers).hide()
